a5a285ea9564a0cac7c67bef3e0114f9ebe3ce3a
Atharva <atharva.tathe55@gmail.com>
2021-04-01 21:06:26 +0530

    Calculate Wages for Month

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -5,29 +5,33 @@ public class EmployeeWages
 		int IS_PRESENT_FULL_TIME = 1;
 		int IS_PRESENT_PART_TIME = 2;
 		int EMP_RATE_PER_HR = 20;
+		int TOTAL_WORKING_DAYS=20;
 		int salary = 0;
 		
 		
-		int empcheck = (int)Math.floor(Math.random() * 10) % 3;
-		
-		switch(empcheck)
+		for(int counter=1; counter<=TOTAL_WORKING_DAYS; counter++)
 		{
-			case 1:
-				int empHrs = 8;
-				salary = empHrs * EMP_RATE_PER_HR ;
-				System.out.println("Full time employee Salary: " +salary);
-				break ;
+		
+			int empcheck = (int)Math.floor(Math.random() * 10) % 3;
+			switch(empcheck)
+			{
+				case 1:
+					int empHrs = 8;
+					salary = empHrs * EMP_RATE_PER_HR ;
+					System.out.println("Full time employee Salary: " +salary);
+					break ;
 			
-			case 2:
-				empHrs = 4;
-				salary = empHrs * EMP_RATE_PER_HR ;
-				System.out.println("Part time employee Salary: " +salary);
-				break;
+				case 2:
+					empHrs = 4;
+					salary = empHrs * EMP_RATE_PER_HR ;
+					System.out.println("Part time employee Salary: " +salary);
+					break;
 			
-			default:
-				salary=0;
-				System.out.println("no Salary: " +salary);
-				break;
+				default:
+					salary=0;
+					System.out.println("no Salary: " +salary);
+					break;
+			}
 		}
 	}
 }
