0f9294f242ff15c93d5c257f2453c83f10c1d9d8
Atharva Tathe <72079840+Atharvatathe@users.noreply.github.com>
2021-04-01 20:59:10 +0530

    Update EmployeeWages.java

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -2,30 +2,8 @@ public class EmployeeWages
 {
  public static void main(String[] args)
 	{
-		int IS_PRESENT_FULL_TIME = 1;
-		int IS_PRESENT_PART_TIME = 2;
-		int EMP_RATE_PER_HR = 20;
-		int salary = 0;
-		
-		double empcheck = Math.floor(Math.random() * 10) % 3;
-		
-		if(IS_PRESENT_FULL_TIME == empcheck)
-		{
-			int empHrs = 8;
-			salary = empHrs * EMP_RATE_PER_HR ;
-			System.out.println("Full time employee Salary: " +salary);
-		}
-		else if(IS_PRESENT_PART_TIME == empcheck)
-		{
-			int empHrs = 4;
-			salary = empHrs * EMP_RATE_PER_HR ;
-			System.out.println("Part time employee Salary: " +salary);
-		}
-		else
-		{
-			salary=0;
-		    System.out.println("no Salary: " +salary);
-		}	
+
+		System.out.println("Welcome to Employee wage computation");
 	}
 }
 
