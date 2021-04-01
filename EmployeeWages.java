f5551315bf73bec703462d125fb92e84ec144b33
Atharva <atharva.tathe55@gmail.com>
2021-04-01 21:02:52 +0530

    Check Part time Employee Wage using Switch case

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -7,25 +7,28 @@ public class EmployeeWages
 		int EMP_RATE_PER_HR = 20;
 		int salary = 0;
 		
-		double empcheck = Math.floor(Math.random() * 10) % 3;
 		
-		if(IS_PRESENT_FULL_TIME == empcheck)
+		int empcheck = (int)Math.floor(Math.random() * 10) % 3;
+		
+		switch(empcheck)
 		{
-			int empHrs = 8;
-			salary = empHrs * EMP_RATE_PER_HR ;
-			System.out.println("Full time employee Salary: " +salary);
+			case 1:
+				int empHrs = 8;
+				salary = empHrs * EMP_RATE_PER_HR ;
+				System.out.println("Full time employee Salary: " +salary);
+				break ;
+			
+			case 2:
+				empHrs = 4;
+				salary = empHrs * EMP_RATE_PER_HR ;
+				System.out.println("Part time employee Salary: " +salary);
+				break;
+			
+			default:
+				salary=0;
+				System.out.println("no Salary: " +salary);
+				break;
 		}
-		else if(IS_PRESENT_PART_TIME == empcheck)
-		{
-			int empHrs = 4;
-			salary = empHrs * EMP_RATE_PER_HR ;
-			System.out.println("Part time employee Salary: " +salary);
-		}
-		else
-		{
-			salary=0;
-		    System.out.println("no Salary: " +salary);
-		}	
 	}
 }
 
